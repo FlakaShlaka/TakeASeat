d0bc0aa992a2e6b5063d45ff961ddd3931c8f997
yasmintiomkin <yasmintiomkin@users.noreply.github.com>
2020-03-27 18:36:58 +0300

    art
    passengers sprite factory
    seats sprite factory
    seat layout

---
+++ Assets/Scripts/PassengerFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassengerFactory : MonoBehaviour
+{
+    public List<Sprite> spriteList = new List<Sprite>();
+
+    private void Start()
+    {
+        InitializeSprite();
+    }
+
+    public void InitializeSprite()
+    {
+        int spriteIndex = Random.Range(0, spriteList.Count);
+        Sprite newSprite = spriteList[spriteIndex];
+
+        SpriteRenderer spriteRendered = gameObject.GetComponent<SpriteRenderer>();
+        spriteRendered.sprite = newSprite;
+    }
+
+}
---
+++ Assets/Scripts/SeatFactory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeatFactory : MonoBehaviour
+{
+    public enum SeatClass { firstclass, business, thisisus};
+    public SeatClass seatClass = SeatClass.firstclass;
+
+    public List<Sprite> spriteList = new List<Sprite>();
+   
+    private void Start()
+    {
+        InitializeSprite();
+    }
+
+    public void InitializeSprite()
+    {
+        int spriteIndex = 0;
+        switch (seatClass)
+        {
+            case SeatClass.firstclass: 
+                spriteIndex = 0;
+                break;
+
+            case SeatClass.business:
+                spriteIndex = 1;
+                break;
+
+            case SeatClass.thisisus:
+                spriteIndex = 2;
+                break;
+        }
+        Sprite newSprite = spriteList[spriteIndex];
+        Debug.Log(spriteIndex);
+
+        SpriteRenderer spriteRendered = gameObject.GetComponent<SpriteRenderer>();
+        spriteRendered.sprite = newSprite;
+    }
+}
