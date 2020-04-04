bfd66ca07267df97dba9ceb7d5adc0157f389ba7
FlakaShlaka <flakashlaka@gmail.com>
2020-04-04 14:52:00 +0300

    Finished Movement

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -17,28 +17,56 @@ public class MoveController : MonoBehaviour
     private enum State { moving, seating, seated };
     private State state;
 
-
+    [Header("Seats Attributes")]
+    private List<Transform> Rows = new List<Transform>(5);
+    private int _selectedRow = 2;
     void Start()
     {
+        GameObject controller = GameObject.Find("Controller");
+
+        Rows.Insert(0, controller.GetComponent<gameController>().Row1);
+        Rows.Insert(1, controller.GetComponent<gameController>().Row2);
+        Rows.Insert(2, controller.GetComponent<gameController>().Aisle);
+        Rows.Insert(3, controller.GetComponent<gameController>().Row3);
+        Rows.Insert(4, controller.GetComponent<gameController>().Row4);
+
         state = State.moving;
+
     }
 
     void Update()
     {
-        switch(state)
+        switch (state)
         {
             //this is the first stage in which the passengers only moving right *constantly*
             case State.moving:
-                
-                    transform.Translate(Vector2.right * Time.deltaTime * speed);
+                transform.Translate(Vector2.right * Time.deltaTime * speed);
+
                 if (Input.GetKeyDown(KeyCode.D))
                 {
-                    transform.Translate(Vector2.right * speed * 10 * Time.deltaTime);
+                    transform.Translate(Vector2.right * speed * 10f * Time.deltaTime);
                 }
-                
-                if(isControlled == true && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S)))
+
+                //If the player leaves the screen
+                if (transform.position.x > 7.4f)
                 {
-                    state = State.seating;
+                    isSeated = true;
+                    state = State.seated;                    
+                }
+
+                if (isControlled == true /*&& (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))*/)
+                {
+                    if(Input.GetKeyDown(KeyCode.W))
+                    {
+                        TakeASeat("w");
+                        state = State.seating;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.S))
+                    {
+                        TakeASeat("s");
+                        state = State.seating;
+                    }
+
                 }
 
                 break;
@@ -46,12 +74,20 @@ public class MoveController : MonoBehaviour
             //this is the state in which we control the player.
             case State.seating:
 
-               TakeASeat();              
-                
-                if(isSeated == true)
+                if (Input.GetKeyDown(KeyCode.W))
                 {
+                    TakeASeat("w");
+                }
+                else if (Input.GetKeyDown(KeyCode.S))
+                {
+                    TakeASeat("s");
+                }
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    isSeated = true;
                     state = State.seated;
                 }
+
                 break;
             
                 //this is the final state.
@@ -63,22 +99,34 @@ public class MoveController : MonoBehaviour
       
     }
 
-    private void TakeASeat()
+    private void TakeASeat(string keyDown)
     {
-        
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            transform.Translate(Vector2.up * Time.deltaTime * speed * seatStep);
-        }
 
-        if (Input.GetKeyDown(KeyCode.S))
+        if (keyDown == "w")
         {
-            transform.Translate(Vector2.down * Time.deltaTime * speed * seatStep);
+            if (_selectedRow == 0)
+            {
+                return;
+                //Top row
+            }
+            else
+            {
+                transform.position = new Vector2(transform.position.x, Rows[_selectedRow - 1].position.y);
+                _selectedRow--;
+            }
         }
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        else if (keyDown == "s")
         {
-            isSeated = true;
+            if (_selectedRow == 4)
+            {
+                return;
+                //Bottom row
+            }
+            else
+            {
+                transform.position = new Vector2(transform.position.x, Rows[_selectedRow + 1].position.y);
+                _selectedRow++;
+            }
         }
     }
 }
--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class gameController : MonoBehaviour
 {
 
+    public Transform Row1;
+    public Transform Row2;
+    public Transform Row3;
+    public Transform Row4;
+    public Transform Aisle;
+
+
     void Start()
     {
-        
+         
     }
 
     void Update()
